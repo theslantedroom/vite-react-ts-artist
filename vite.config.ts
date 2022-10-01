f5bab4b6cfd32e09fe3e24e6d9e74c4596b085d2
Steve Yee <steveplaysaxe@hotmail.com>
2022-10-01 09:18:27 -0700

    push update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-ts-artist/',
+  // base: '/vite-react-ts-artist/',
+  base: '/',
 });
 
 // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
